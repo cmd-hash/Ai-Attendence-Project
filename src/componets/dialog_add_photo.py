56e4aeb91b225dd35f02edb0d7eb087fa66554b5
cmd-hash <nrpssoron1@gmail.com>
2026-05-23 12:57:07 +0530

    fix: attendance spelling in dialog_add_photo

--- src/componets/dialog_add_photo.py
+++ src/componets/dialog_add_photo.py
@@ -20,8 +20,6 @@ def add_photos_dialog():
         if st.button('Camera', type=type_camera, width='stretch'):
             st.session_state.photo_tab = 'camera'
 
-
-
     with t2:
         type_upload = "primary" if st.session_state.photo_tab == 'upload' else 'tertiary'
         if st.button('Upload photos', type=type_upload, width='stretch'):
@@ -30,18 +28,22 @@ def add_photos_dialog():
     if st.session_state.photo_tab == 'camera':
         cam_photo = st.camera_input('Take Snapshot', key='dialog_cam')
         if cam_photo:
-            st.session_state.attendence_images.append(Image.open(cam_photo))
+            st.session_state.attendance_images.append(Image.open(cam_photo))
             st.toast('Photo Captured')
             st.rerun()
 
-
     if st.session_state.photo_tab == 'upload':
-        uploaded_files = st.file_uploader( 'choose image files', type=['jpg', 'png', 'jpeg' ], accept_multiple_files=True, key='dialog_upload')
+        uploaded_files = st.file_uploader(
+            'choose image files',
+            type=['jpg', 'png', 'jpeg'],
+            accept_multiple_files=True,
+            key='dialog_upload'
+        )
 
         if uploaded_files:
             for f in uploaded_files:
-                st.session_state.attendence_images.append(Image.open(f))
-            
+                st.session_state.attendance_images.append(Image.open(f))
+
             st.toast('Photo Uploaded Successfully')
             st.rerun()
 
